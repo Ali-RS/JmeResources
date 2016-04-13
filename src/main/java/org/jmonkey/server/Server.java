434cc6df081bed1dd6b775d78011c3bbd887adb2
jayfella <jayfella@gmail.com>
2016-04-13 18:26:41 +0100

    Remove unused import and diamond operator.

--- src/main/java/org/jmonkey/server/Server.java
+++ src/main/java/org/jmonkey/server/Server.java
@@ -99,7 +99,7 @@ public class Server extends org.eclipse.jetty.server.Server {
     {
         JettyJasperInitializer sci = new JettyJasperInitializer();
         ContainerInitializer initializer = new ContainerInitializer(sci, null);
-        List<ContainerInitializer> initializers = new ArrayList<ContainerInitializer>();
+        List<ContainerInitializer> initializers = new ArrayList<>();
         initializers.add(initializer);
         return initializers;
     }
