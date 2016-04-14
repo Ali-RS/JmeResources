10d6e77ffd9364f077d35102d65b9e17be8874b3
jayfella <jayfella@gmail.com>
2016-04-14 12:22:04 +0100

    Add crazy class-loading wizardry to get jstl to work properly.

--- src/main/java/org/jmonkey/server/Server.java
+++ src/main/java/org/jmonkey/server/Server.java
@@ -21,10 +21,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
+import java.util.jar.Manifest;
+import org.eclipse.jetty.webapp.WebAppClassLoader;
 
 /**
  * @author jayfella
@@ -47,12 +52,13 @@ public class Server extends org.eclipse.jetty.server.Server {
         htmlServlet.setThrowExceptionIfNoHandlerFound(true);
 
         WebAppContext contextHandler = new WebAppContext();
+        this.setClassLoader(contextHandler);
+        
         contextHandler.setAttribute("javax.servlet.context.tempdir", this.getScratchDir());
         contextHandler.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/.*taglibs.*\\.jar$");
         contextHandler.setAttribute("org.eclipse.jetty.containerInitializers", jspInitializers());
         contextHandler.setAttribute(InstanceManager.class.getName(), new SimpleInstanceManager());
         contextHandler.addBean(new ServletContainerInitializersStarter(contextHandler), true);
-        contextHandler.setClassLoader(getUrlClassLoader());
 
         contextHandler.setContextPath(config.getBindPath().isEmpty() ? "/" : config.getBindPath());
         contextHandler.setResourceBase(new ClassPathResource("webapp").getURI().toString());
@@ -89,12 +95,6 @@ public class Server extends org.eclipse.jetty.server.Server {
         this.setConnectors(new Connector[] { httpConnector });
     }
 
-    private ClassLoader getUrlClassLoader()
-    {
-        ClassLoader jspClassLoader = new URLClassLoader(new URL[0], this.getClass().getClassLoader());
-        return jspClassLoader;
-    }
-
     private List<ContainerInitializer> jspInitializers()
     {
         JettyJasperInitializer sci = new JettyJasperInitializer();
@@ -119,4 +119,42 @@ public class Server extends org.eclipse.jetty.server.Server {
         return scratchDir;
     }
 
+    // @see http://stackoverflow.com/a/29037344
+    private void setClassLoader(WebAppContext context) throws MalformedURLException, IOException {
+        
+        //========== create WebAppClassloader with a new parent classloader with all URLs in manifests=================== 
+        //search for any secondary class path entries
+        Vector<URL> secondayClassPathURLVector = new Vector<>();
+        ClassLoader parentClassLoader = this.getClass().getClassLoader();
+        if(parentClassLoader instanceof URLClassLoader)
+        {
+            URL[] existingURLs = ((URLClassLoader)parentClassLoader).getURLs(); 
+            for (URL parentURL : existingURLs)
+            {
+                //if it doesn't end in .jar, then it's probably not going to have Manifest with a Class-Path entry 
+                if(parentURL.toString().endsWith(".jar"))
+                {
+                    JarURLConnection jarURLConnection = (JarURLConnection) new URL("jar:" + parentURL.toString() + "!/").openConnection();
+                    Manifest jarManifest = jarURLConnection.getManifest();
+                    String classPath = jarManifest.getMainAttributes().getValue("Class-Path");
+                    if(classPath != null)
+                    {
+                        //Iterate through all of the class path entries and create URLs out of them                                                
+                        for (String part : classPath.split(" "))
+                        {
+                            //add our full path to the jar file to our classpath list
+                            secondayClassPathURLVector.add(new URL(parentURL,part));                     
+                        }
+                    }
+                }                
+            }
+        }
+        //use our class path entries to create a parent for the webappclassloader that knows how to use or referenced class paths
+        URLClassLoader internalClassPathUrlClassLoader = new URLClassLoader(secondayClassPathURLVector.toArray(new URL[secondayClassPathURLVector.size()]), parentClassLoader);
+        //create a new webapp class loader as a child of our  internalClassPathUrlClassLoader. For whatever reason Jetty needs to have a WebAppClassLoader be it's main class loader,
+        //while all of our classpath entries need to be added to the parent class loader   
+        WebAppClassLoader webAppClassLoader = new WebAppClassLoader(internalClassPathUrlClassLoader,context);
+        context.setClassLoader(webAppClassLoader);
+    }
+    
 }
